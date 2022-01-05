60986822339ccbce68335b8157614267dff48f45
BrandonMerry <Brandon.l.merry@gmail.com>
2022-01-05 19:15:29 +0700

    classlist.add + classlist.remove

--- modal_pop_out/app.js
+++ modal_pop_out/app.js
@@ -1,4 +1,21 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
+// select modal-btn,modal-overlay,close-btn 
+const modalBtn = document.querySelector('.modal-btn');
+const overlay = document.querySelector ('.modal-overlay');
+const closeBtn = document.querySelector ('.close-btn');
+
+
+
+// listen for click events on modal-btn 
 // when user clicks modal-btn add .open-modal to modal-overlay
+modalBtn.addEventListener('click', function(){
+    overlay.classList.add("open-modal");
+});
+
+// listen for click events on close-btn
 // when user clicks close-btn remove .open-modal from modal-overlay
+closeBtn.addEventListener('click', function(){
+    overlay.classList.remove("open-modal");
+});
+
+
+
