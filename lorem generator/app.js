7c880bd2495a2010f5fb4094613c724517669d04
Brandon Merry <Brandon.l.merry@gmail.com>
2022-02-17 00:17:57 +0700

    learned .join and isNan for JS

--- lorem generator/app.js
+++ lorem generator/app.js
@@ -1,4 +1,4 @@
-// lorem text
+// an array of 9 items. each item is a paragraph  
 const text = [
     `Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.`,
     `Next level tbh everyday carry, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.`,
@@ -11,3 +11,51 @@ const text = [
     `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
     `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
   ];
+
+//   use the class when you use query selector 
+  const form = document.querySelector(".lorem");
+//   input has no class, only an ID use 
+  const amount = document.getElementById("amount");
+// this is the article where your lorem will "print"
+  const result = document.querySelector(".lorem-text"); 
+
+
+//   now you need to listen for an even to show JS it needs to start working 
+// this is a submit event, so its wrapped in a form. 
+// default behavior is to submit to a server  -- need to prevent for tis 
+form.addEventListener("submit", function(e){
+    // stops submission to server
+    e.preventDefault();
+
+    // access the input and change from string to number with parsInt
+    const value = parseInt(amount.value);
+    const random = Math.floor(Math.random() * text.length);
+
+
+     // consol.log(typeof value); do this to check your work (strings === black, number === blue)
+    //  console.log(isNaN(value));
+    // now set up if statement to cover the options - places one p for each item below
+    // empty value (less or equal to 0) -- isNaN (not a )
+    // negative number -- value < 0
+    // larger than 9 -- value > 9
+
+    // value is your const from above
+    
+    if (isNaN(value) || value <= 0 || value > 9) {
+        result.innerHTML = `<p class="result">${text[random]}</p>`;
+    }else {
+        // use slice for starting at 0 and the end => const=value (end at 9)
+        // this is how to put 
+        let tempText = text.slice(0, value);
+        // this selects items from the array 
+        tempText = tempText.map(function (item) {
+            return `<p class="result">${item}</p>`;
+          })
+        //   the join method adds multiple items together 
+          .join("");
+
+        //   now this takes the const = result (the html class of "lorem-text") and inserts tempText inside the article HTML tag
+        result.innerHTML = tempText;
+      }
+    });
+    
