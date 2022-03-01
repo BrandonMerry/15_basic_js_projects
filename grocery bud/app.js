a6d09d740943cbcefeaf1ea25812367bfb5ddee9
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-01 19:17:49 +0700

    created all const for JS

---
+++ grocery bud/app.js
@@ -0,0 +1,20 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert')
+const alert = document.querySelector('.grocery-form')
+const grocery = document.getElementById('grocery')
+const submtBtn = document.querySelector('.submit-btn')
+const container = document.querySelector('.grocery-container')
+const list = document.querySelector('.grocery-list')
+const clearBtn = document.querySelector('.clear-btn')
+
+
+
+// edit option
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
